9652f6615c3a7dd1e17a11ff4ff3f8f2d2e0c13a
proenix12 <georgi.nqgolov@gmail.com>
2018-11-01 17:19:07 +0200

    Add walker class example

--- goliveuk/functions.php
+++ goliveuk/functions.php
@@ -115,3 +115,109 @@ function wpdocs_excerpt_more( $more ) {
     return '<a href="'.get_the_permalink().'" class="read-more">...Read More</a>';
 }
 add_filter( 'excerpt_more', 'wpdocs_excerpt_more' );
+
+
+//function change_submenu_class($menu) {
+//    $menu = preg_replace('/ class="sub-menu"/','/ class="myclass" /',$menu);
+//    return $menu;
+//}
+//add_filter('wp_nav_menu','change_submenu_class');
+
+
+//class My_Walker_Nav_Menu extends Walker_Nav_Menu {
+//		function display_element ( $element, &$children_elements, $max_depth, $depth = 0, $args, &$output ) {
+//				$GLOBALS['dd_children'] = ( isset( $children_elements[ $element->ID ] ) ) ? 1 : 0;
+//				$GLOBALS['dd_depth']    = (int) $depth;
+//				parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
+//		}
+//
+//		function start_lvl ( &$output, $depth = 0, $args = [] ) {
+//				$indent = str_repeat( "\t", $depth );
+//				$output .= "\n$indent<div class=\"sub-menu\"><ul class=\"dropdown-menu\">\n";
+//		}
+//
+//		function start_el ( &$output, $item, $depth = 0, $args = [], $id = 0 ) {
+//				global $wp_query, $wpdb;
+//				$indent        = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+//				$li_attributes = '';
+//				$class_names   = $value = '';
+//				$classes       = empty( $item->classes ) ? [] : (array) $item->classes;
+//
+//				//Add class and attribute to LI element that contains a submenu UL.
+//				if ( isset( $args->has_children ) ) {
+//						$classes[]     = 'dropdown';
+//						$li_attributes .= 'data-dropdown="dropdown"';
+//				}
+//				$classes[] = 'menu-item-' . $item->ID;
+//				//If we are on the current page, add the active class to that menu item.
+//				$classes[] = ( $item->current ) ? 'active' : '';
+//				//Make sure you still add all of the WordPress classes.
+//				$class_names  = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+//				$class_names  = ' class="' . esc_attr( $class_names ) . '"';
+//				$id           = apply_filters( 'nav_menu_item_id', 'menu-item-' . $item->ID, $item, $args );
+//				$id           = strlen( $id ) ? ' id="' . esc_attr( $id ) . '"' : '';
+//				$has_children = $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(*) FROM $wpdb->postmeta WHERE meta_key = %s AND meta_value = %d", '_menu_item_menu_item_parent', $item->ID ) );
+//				$output       .= $indent . '<li' . $id . $value . $class_names . '>';
+//				$output       .= $indent . '<li' . $id . $value . $class_names . $li_attributes . '>';
+//				//Add attributes to link element.
+//				$attributes = ! empty( $item->attr_title ) ? ' title="' . esc_attr( $item->attr_title ) . '"' : '';
+//				$attributes .= ! empty( $item->target ) ? ' target="' . esc_attr( $item->target ) . '"' : '';
+//				$attributes .= ! empty( $item->xfn ) ? ' rel="' . esc_attr( $item->xfn ) . '"' : '';
+//				$attributes .= ! empty( $item->url ) ? ' href="' . esc_attr( $item->url ) . '"' : '';
+//				// Check if menu item is in main menu
+//
+//				if ( $depth == 0 && $has_children > 0 ) {
+//						// These lines adds your custom class and attribute
+//						$attributes .= ' class="dropdown-toggle"';
+//						$attributes .= ' data-toggle="dropdown"';
+//				}
+//				$item_output = $args->before;
+//				$item_output .= '<a' . $attributes . '>';
+//				$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+//				// Add the caret if menu level is 0
+//				if ( $depth == 0 && $has_children > 0 ) {
+//						$item_output .= ' <b class="caret"></b>';
+//				}
+//				$item_output .= '</a>';
+//				$item_output .= $args->after;
+//				$output      .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+//		}
+//
+//		function end_lvl ( &$output, $depth = 0, $args = [] ) {
+//				$indent = str_repeat( "\t", $depth );
+//				$output .= "$indent</ul></div>\n";
+//		}
+//}
+//
+//
+//if ( function_exists( 'register_sidebars' ) ) {
+//		register_sidebar( [
+//				'name'         => 'Left Sidebar',
+//				'before_title' => '<h2 class="label">',
+//				'after_title'  => '</h2>',
+//				'description'  => 'Items placed here will be shown in the left sidebar.'
+//		] );
+//}
+
+class My_Walker_Nav_Menu extends Walker_Nav_Menu {
+		function start_el( &$output, $item, $depth = 0, $args = [], $id = 0 ) {
+				if ( array_search( 'menu-item-has-children', $item->classes ) ) {
+						$output .= sprintf( "\n<li class='item haschild  %s'>
+<a href='#' class=\"\" data-toggle=\"dropdown\" >$item->title</a>\n",
+								( array_search( 'current-menu-item', $item->classes ) ||
+								  array_search( 'current-page-parent', $item->classes ) ) ? 'active' : '', $item->url, $item->title );
+				} else {
+						$output .= sprintf( "\n<li class='item' %s><a href='%s'>%s</a>\n", ( array_search( 'current-menu-item', $item->classes) ) ? ' class="active"' : '', $item->url, $item->title );
+				}
+		}
+		
+		function start_lvl( &$output, $depth = 0, $args = [] ) {
+				$indent = str_repeat( "\t", $depth );
+				$output .= "\n$indent<div class=\"sub-menu\"><ul class=\"dropdown-menu\" role=\"menu\">\n";
+		}
+		
+		function end_lvl ( &$output, $depth = 0, $args = [] ) {
+				$indent = str_repeat( "\t", $depth );
+				$output .= "\n$indent</ul></div>\n";
+		}
+}
